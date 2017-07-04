a0f55b9ffa05e97e11649b3a9fbf10029e512429
IRFAN KHAN <irfan972588@gmail.com>
2017-07-04 11:19:49 +0530

    user info file added

---
+++ get_user_info.py
@@ -0,0 +1,14 @@
+import requests
+from constants import *
+from get_user_id import *
+
+
+def get_user_info():
+    #function logic here
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
