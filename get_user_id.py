e2f0cb9ba1a4b768423488058a02e13540c53f85
IRFAN KHAN <irfan972588@gmail.com>
2017-07-04 11:14:54 +0530

    user id file added

---
+++ get_user_id.py
@@ -0,0 +1,9 @@
+import requests
+from constants import *
+
+
+def get_user_id(insta_username):
+    #function logic
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
