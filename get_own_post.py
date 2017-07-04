f6715edd9d6b93f8e3bf758792d3eb70a99c1b22
IRFAN KHAN <irfan972588@gmail.com>
2017-07-04 12:17:27 +0530

    get own post function added

---
+++ get_own_post.py
@@ -0,0 +1,7 @@
+import requests
+from constants import *
+
+def get_own_post():
+
+    request_url = (BASE_URL + "")
+    print("")
---
+++ main.py
@@ -0,0 +1,12 @@
+#main file
+from self_info  import self_info
+from get_user_info import *
+from get_own_post import get_own_post
+#fetching own info.
+
+
+
+self_info()
+get_user_info()
+result = get_own_post()
+print (result)
