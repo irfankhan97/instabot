27bbf13b9bfa59a009e58b8629eb6f0eb33ddbb9
IRFAN KHAN <irfan972588@gmail.com>
2017-07-04 20:10:20 +0530

    main.py modified

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-#main file
-from self_info  import self_info
-from get_user_info import *
+#main.py file initiated.
+from self_info import self_info
 from get_own_post import get_own_post
-#fetching own info.
+#fetching own information
+#self_info()
 
-
-
-self_info()
-get_user_info()
-result = get_own_post()
-print (result)
+get_own_post()
