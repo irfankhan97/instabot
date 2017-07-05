c0a8893a7eb0f43f581696e0f963a2bd41833de4
IRFAN KHAN <irfan972588@gmail.com>
2017-07-05 23:09:18 +0530

    main.py modified

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ while show_menu:
     menu_choices = "What do you want to do? \n 1. Like A Post \n 2. Comment on a post \n 3. Download Own Post \n 4. Download Friend's post \n 5. Get Friend Info. \n 6. Close Application \n"
     menu_choice = input(menu_choices)
 
+
+
     if (menu_choice) > 0:
         menu_choice = int(menu_choice)
 
         if menu_choice == 1:
             insta_username = raw_input("Enter Username.........\n")
+            print("Wait liking Ur POst......")
             like_user_post(insta_username)
+            print("\n")
+            print("\n")
 
         elif menu_choice == 2:
             insta_username = raw_input("Enter Username.........\n")
             comment_user_post(insta_username)
+            print("wait work under process.......")
+            print("\n")
+            print("\n")
         elif menu_choice == 3:
             print ("WAit Getting ur post.......\n")
             get_own_post()
+            print("\n")
+            print("\n")
         elif menu_choice == 4:
             insta_username = raw_input("Enter Username.........\n")
+            print("Wait Downloading user post......")
             get_user_post(insta_username)
+
+            print("\n")
+            print("\n")
         elif menu_choice == 5:
             insta_username = raw_input("Enter Username.........\n")
+            print("Wait getting information.....")
             get_user_info(insta_username)
+
+            print("\n")
+            print("\n")
         else:
             show_menu = False
