2bb8485217bdd3dc1bf695464038bc358dee4d85
IRFAN KHAN <irfan972588@gmail.com>
2017-07-05 10:28:30 +0530

    lilke post file is added

---
+++ like_user_post.py
@@ -0,0 +1,19 @@
+import requests
+from constants import APP_ACCESS_TOKEN ,BASE_URL
+from get_user_id import get_user_id
+
+username = "irfan"
+
+def like_user_post(insta_username):
+    media_id = get_user_id(insta_username)
+    request_url = (BASE_URL + "/media/&s/likes") % (media_id)
+    payload = {"access_token" : APP_ACCESS_TOKEN}
+    post_a_like = requests.post(request_url,payload).json()
+    if post_a_like['meta']['code']== 200:
+        print("Post liked successfully")
+    else :
+        print('User does not exist')
+
+like_user_post(username)
+
+
